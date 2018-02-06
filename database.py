9442bede530d31465dfe60be34ec61bed3e1602d
Gregory Petukhov <lorien@lorien.name>
2018-02-06 15:28:05 +0300

    Add index to event collection

--- database.py
+++ database.py
@@ -4,4 +4,5 @@ from pymongo import MongoClient
 def connect_db():
     db = MongoClient()['daysandbox']
     db.user.create_index('username', unique=True)
+    db.event.create_index([('type', 1), ('date', 1)])
     return db
